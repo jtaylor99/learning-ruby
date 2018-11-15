2cd5bb2715ecb709bb23df1e0a321a26094d2448
jtaylor99 <jtaylor@basecampcodingacademy.org>
2018-11-15 10:40:18 -0600

    Adding an item to an array

--- pratice.rb
+++ pratice.rb
@@ -2,5 +2,7 @@ student_1 = "Harry Potter"
 student_2 = "Ron Weasly"
 student_3 = "Hermione Granger"
 student_4 = "Draco Malfoy"
+student_5 = "Justice Taylor"
 students = [student_1,student_2,student_3,student_4]
-puts students[0]
+students << student_5
+puts students[4]
