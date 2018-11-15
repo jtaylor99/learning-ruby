60cee79e9c017e45de0394624529dc2df8b68522
jtaylor99 <jtaylor@basecampcodingacademy.org>
2018-11-15 10:33:07 -0600

    Example 1

---
+++ pratice.rb
@@ -0,0 +1,6 @@
+student_1 = "Harry Potter"
+student_2 = "Ron Weasly"
+student_3 = "Hermione Granger"
+student_4 = "Draco Malfoy"
+students = [student_1,student_2,student_3,student_4]
+puts students[0]
