501513261fdb10ab0e8526653c418d400a2b1125
jtaylor99 <jtaylor@basecampcodingacademy.org>
2018-11-15 11:33:05 -0600

    Intro to Methods

--- pratice.rb
+++ pratice.rb
@@ -1,8 +1,22 @@
-student_1 = "Harry Potter"
-student_2 = "Ron Weasly"
-student_3 = "Hermione Granger"
-student_4 = "Draco Malfoy"
-student_5 = "Justice Taylor"
-students = [student_1,student_2,student_3,student_4]
-students << student_5
-puts students[4]
+# student_1 = "Harry Potter"
+# student_2 = "Ron Weasly"
+# student_3 = "Hermione Granger"
+# student_4 = "Draco Malfoy"
+# student_5 = "Justice Taylor"
+# students = [student_1,student_2,student_3,student_4]
+# students << student_5
+# puts students.include?("Nate")
+def say_hello_ten_times
+    phrase = "Hello World"
+    puts phrase
+    puts phrase
+    puts phrase
+    puts phrase
+    puts phrase
+    puts phrase
+    puts phrase
+    puts phrase
+    puts phrase
+    puts phrase
+end
+say_hello_ten_times
